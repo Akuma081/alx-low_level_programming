ef2bccbfea96d8819d32dcce44731a0335f9504e
Akuma081 <chukwuakuma2020@gmail.com>
2023-07-17 00:55:09 -0700

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - this is the main function
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	int x;
+
+	for (x = 0; x < 10; x++)
+		printf("%d",x);
+	printf("\n");
+	return (0);
+}
