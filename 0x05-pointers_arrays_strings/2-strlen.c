855e10df130246cba11369d10969d346222173dd
Akuma081 <chukwuakuma2020@gmail.com>
2023-07-24 13:40:52 -0700

    2-strlen.c

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,22 @@
+nclude "main.h"
+
+/**
+ * _strlen - a function that returns the length of a string
+ *
+ * @s: string parameter
+ *
+ * Return: 0 (String)
+ */
+
+int _strlen(char *s)
+{
+	int x;
+
+	x = 0;
+	while (*s != '\0')
+	{
+		x++;
+		s++;
+	}
+	return (x);
+}
