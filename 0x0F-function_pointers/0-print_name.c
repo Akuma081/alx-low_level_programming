1e75ad8363ab77811891b9504ee9d2a0f3282355
Akuma081 <chukwuakuma2020@gmail.com>
2023-08-16 09:25:33 -0700

    0-print_name.c

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,19 @@
+#include "function_pointers.h"
+#include <stdio.h>
+
+/**
+ * print_name - prints the name
+ *
+ * @name: char to display to the stdout
+ *
+ * @f: apointer function
+ *
+ * Return: 0 (Success)
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	if (name == NULL || f == NULL)
+		return;
+	(*f)(name);
+}
